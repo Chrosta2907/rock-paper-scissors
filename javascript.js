be94794f361334f14fd9774d9ade7273230b63e6
Hobbit <mateuszchroscinski@gmail.com>
2023-02-18 00:29:14 +0100

    Add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+let computerChoice
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    console.log(randomNumber)
+    if (randomNumber === 0) {
+        computerChoice = "Rock"
+    }
+    else if (randomNumber === 1) {
+        computerChoice = "Paper"
+    }
+    else {computerChoice = "Scissors"}
+    
+    return computerChoice
+}
+console.log(getComputerChoice())
